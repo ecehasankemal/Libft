2edf5a631555b3904ac78a45c64809e431cc8b6a
Hasan Kemal ECE <109526257+ecehasankemal@users.noreply.github.com>
2022-09-23 15:15:33 +0300

    Add files via upload

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hece  <hece@student.42kocaeli.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 17:15:15 by hece              #+#    #+#             */
-/*   Updated: 2022/09/19 20:17:47 by hece             ###   ########.tr       */
+/*   Updated: 2022/09/23 12:42:20 by hece             ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,7 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
-void	ft_lstclear(t_list **lst, void (*del)(vold *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
