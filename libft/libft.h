59963b9988ad55d78ab3e186ddb8ce75f8b103a8
Hasan Kemal ECE <109526257+ecehasankemal@users.noreply.github.com>
2022-09-26 01:15:13 +0300

    Add files via upload

--- libft/libft.h
+++ libft/libft.h
@@ -48,6 +48,7 @@ char	*ft_substr(const char *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_itoa(int n);
+char	**ft_split(char const *s, char c);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
